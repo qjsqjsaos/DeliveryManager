63b939e588fd04977ddb9cc376fa3d9520028ba7
souyeolBaek <merrygoaround0726@gmail.com>
2022-06-22 22:57:52 +0900

    진행중 - MainActivity - 안드로이드 12 스플래시 화면 구현

--- app/src/main/java/com/sooyeol/deliverymanager/ui/MainActivity.kt
+++ app/src/main/java/com/sooyeol/deliverymanager/ui/MainActivity.kt
@@ -5,17 +5,14 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.sooyeol.deliverymanager.R
-import com.sooyeol.deliverymanager.ui.splash.SplashActivity
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // TODO:  https://dnight.tistory.com/entry/Android-12-Splash-Screen 스플래시 스크린 참고 후에 12버전 이전에 마이그레이션을 적용하자
-
     }
 }
